163c3c43520e32d5d8e35f2bb1a0e81e85a6b278
Anaistat <90132088+Anaistat@users.noreply.github.com>
2021-11-09 20:41:55 +0300

    fix: fix mistakes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,29 +1,23 @@
-function copyDir(){
+function copyFolderFiles(){
     const fs = require('fs');
     const path = require('path');
-
-    fs.mkdir(path.join(__dirname, 'files-copy'),
-        { recursive: true }, (err) => {
-            if (err) {
-                return console.error(err);
-            }
-            fs.readdir(path.resolve(__dirname, 'files'), function (error, files) {
-                if (error) {
-                    console.error('Could not list the directory.');
-                }
-                else{
-
-                    files.forEach((file)=>{
-                        fs.copyFile(path.join(__dirname, 'files', file), (path.join(__dirname, 'files-copy', file)), (err)=>{
-                            if(err) throw err
-                            else{
-                                console.log("File copied successfully")
-                            }
+    fs.rmdir(path.resolve(__dirname, 'files-copy'), { recursive: true }, () => {
+        console.log(path.resolve(__dirname, 'files-copy'))
+        fs.mkdir(path.join(__dirname, 'files-copy'),
+            { recursive: true }, (err) => {
+                if (err) throw err
+                fs.readdir(path.resolve(__dirname, 'files'), function (error, files) {
+                    if (error) throw error
+                    else {
+                        files.forEach((file) => {
+                            fs.copyFile(path.join(__dirname, 'files', file), (path.join(__dirname, 'files-copy', file)), (err)=>{
+                                if(err) throw err
+                            })
                         })
-                    })
-                }
-            })
-        });
+                    }
+                })
+            });
+    })
 }
 
-copyDir()
+copyFolderFiles()
