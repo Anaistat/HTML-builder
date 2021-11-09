f111ef45b858ca9303cb010919c2ba1d63daae5b
Anaistat <90132088+Anaistat@users.noreply.github.com>
2021-11-09 20:38:31 +0300

    fix: fix mistakes

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,33 +1,37 @@
 const readline = require('readline')
 const fs = require('fs')
 
-
-console.log('Hello, stranger')
+console.log('Hello,stranger')
 
 const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 })
 
-rl.question('What do you want to write to a file? ', (answer) => {
 
-  fs.appendFile('02-write-file/text.txt', answer, function(error){
-    if(error) throw error
-    console.log('\nDone')
-  });
+function input (queryText = '') {
+  rl.question(queryText, (answer) => {
+    fs.appendFile('02-write-file/text.txt', answer, function(error){
+      if(error) throw error
+    })
+    input()
+  })
+}
+input('What do you want to write to a file? ')
 
-  rl.close()
-})
+
+
+
 
 rl.on('SIGINT', () => {
-  console.log('\nInput exit...')
+  console.log('\nAll done.')
   rl.close()
 })
-let text = ''
+let textWrite = ''
 process.stdin.on('keypress', (c, k) => {
-  text += k.sequence
-  if (text.includes('exit')) {
-    console.log('\nInput exit.')
+  textWrite += k.sequence
+  if (textWrite.includes('exit')) {
+    console.log('\nAll done.')
     rl.close()
   }
 });
